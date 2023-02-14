963649c0135b0a03db91686a64ef0edc6847685d
Jebas Winston E <jebaswinston55@gmail.com>
2023-02-14 11:37:33 +0530

    Adding Exception to Question 7.2

--- Question7.py
+++ Question7.py
@@ -11,14 +11,24 @@ seven_multiples()
          2. Assume name and marks provided will be of same length
          3. There are two lists name=[ "rahul", "aswin", "abi"] marks = [80, 90, 70]'''
 
-def create_dict(names, marks):
-    my_dictionary = {}
+class KeyAlreadyPresentException(Exception):
+    pass
+
+def create_dict(names, marks, my_dictionary = {}):
     for name,mark in zip(names, marks):
-        my_dictionary[f'{name}'] = mark
+        try:
+            if name not in my_dictionary.keys():
+                my_dictionary[f'{name}'] = mark
+            else:
+                raise KeyAlreadyPresentException              
+        except KeyAlreadyPresentException:
+            print("Key already present")
+            break
     return my_dictionary
 
-names = [ "rahul", "aswin", "abi"]
-marks = [80, 90, 70]
+my_dictionary = {}
+names = ["rahul", "aswin", "abi", "abi"]
+marks = [80, 90, 70, 70]
 print(f"Names are: {names}")
 print(f"Marks are: {marks}")
 print(f"The Dictionary is: {create_dict(names, marks)}")
