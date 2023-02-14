b6eeb057ed002ea67189cdde5106e1f761fd9e40
Jebas Winston E <jebaswinston55@gmail.com>
2023-02-14 10:25:09 +0530

    Adding Question 7.1

---
+++ Question7.py
@@ -0,0 +1,7 @@
+''' Print the multiples of 7 from 147 to 210 '''
+def seven_multiples():
+    print("The multiples of seven from 147 to 210 are: ")
+    for num in range(147,211,7):
+        print(num)
+
+seven_multiples()
