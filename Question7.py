51756c3772a6674053e06d5700634953d960bb59
Jebas Winston E <jebaswinston55@gmail.com>
2023-02-14 10:35:56 +0530

    Adding Question 7.2

--- Question7.py
+++ Question7.py
@@ -5,3 +5,20 @@ def seven_multiples():
         print(num)
 
 seven_multiples()
+
+''' Create a dictinary with whose keys are names and values are their marks. 
+    Note 1. The names can be of any length 
+         2. Assume name and marks provided will be of same length
+         3. There are two lists name=[ "rahul", "aswin", "abi"] marks = [80, 90, 70]'''
+
+def create_dict(names, marks):
+    my_dictionary = {}
+    for name,mark in zip(names, marks):
+        my_dictionary[f'{name}'] = mark
+    return my_dictionary
+
+names = [ "rahul", "aswin", "abi"]
+marks = [80, 90, 70]
+print(f"Names are: {names}")
+print(f"Marks are: {marks}")
+print(f"The Dictionary is: {create_dict(names, marks)}")
