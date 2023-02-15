62b60feed4854e6e89baf39fabd79cbe67b44843
Jebas Winston E <jebaswinston55@gmail.com>
2023-02-15 12:09:43 +0530

    Changes in Question 1

--- Question1.py
+++ Question1.py
@@ -1,11 +1,11 @@
 ''' Validate a IP address using python Regex '''
 import re
 def validate(ip_address):
-    regex = r"[^0][0-255]{1,3}.[^0][0-255]{1,3}.[^0][0-255]{1,3}.[^0][0-255]{1,3}"
+    regex = r"[0-255]{1,3}\.[0-255]{1,3}\.[0-255]{1,3}\.[1-255]{1,3}"
     if re.search(regex,ip_address) != None:
         print("Your IP Address is correct!")
         print(re.findall(regex,ip_address))
     else:
         print("Your IP Address is incorrect!")
 
-validate("255.255.255.1")
+validate("255.255.255.10")
